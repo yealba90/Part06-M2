0d932d7dcfa2a74387161b3ce79f7ccc376a2a92
Yeison Alvarez Balvin <ya.balvin1990@gmail.com>
2022-05-11 17:30:34 -0500

    Solving Homework

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,21 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+  <div>
+    <span>{props.name}</span>
+    <button onClick={props.onClose}>X</button>
+    <div>
+      <div>
+        <p>MIN</p>
+        <p>{props.min}</p>
+      </div>
+      <div>
+        <p>MAX</p>
+        <p>{props.max}</p>
+      </div>
+      <img src={`http://openweathermap.org/img/wn/02d@2x.png`} />
+    </div>
+  </div>
+  )
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,11 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+  <form>
+  <input placeholder='Ciudad...' /> 
+  <input type="submit" value="Agregar" /> 
+  </form>
+  
+  )
 };
