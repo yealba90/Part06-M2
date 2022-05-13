e27111b817656a32722031d36c4c111423d1c089
Yeison Alvarez Balvin <ya.balvin1990@gmail.com>
2022-05-12 21:40:18 -0500

    Solving Homework

--- 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
@@ -3,9 +3,13 @@ import Logo from '../logoHenry.png'
 import SearchBar from './SearchBar.jsx';
 import './Nav.css';
 
-function Nav({onSearch}) {
+function Nav({ onSearch }) {
   return (
-    <div>Nav</div>
+    <div className='nav-header'>
+      <img src={Logo} alt='Logo Henry'/>
+      <h3 className='nav-title'>Henry - Weather App</h3>
+      <SearchBar onSearch={onSearch} />
+    </div>
   );
 };
 
--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
+import s from './SearchBar.module.css';
 
 export default function SearchBar({onSearch}) {
+  const [city, setCity] = useState('');
   return (
-    <form onSubmit={(e) => {
+    <form className={s.container}
+    onSubmit={(e) => {
       e.preventDefault();
-      onSearch("Cairns");
+      onSearch(city);
+      setCity('');
     }}>
       <input
         type="text"
         placeholder="Ciudad..."
+        onChange={e => setCity(e.target.value)}
+        value={city}
       />
-      <input type="submit" value="Agregar" />
+      <input className={s.btnSearch} type="submit" value="Agregar" />
     </form>
   );
 }
+
+
+
+// import React from 'react';
+// import s from './SearchBar.module.css';
+
+// export default function SearchBar({onSearch}) {
+//   // acá va tu código
+//   return (
+//   <div className={s.container}>
+//     <input type='text' placeholder='Ingrese Ciudad...'/> 
+//     <button className={s.btnSearch} onClick={() => onSearch('Agregando ciudad...')}>Agregar</button> 
+//   </div>
+  
+//   )
+// };
