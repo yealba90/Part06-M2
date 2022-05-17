1cc03ef53b25736ef12dd8a0cc89537cf5f955eb
Yeison Alvarez Balvin <ya.balvin1990@gmail.com>
2022-05-17 16:33:15 -0500

    Solving Homework

--- 10-React-Forms/demo/app.js
+++ 10-React-Forms/demo/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Form from './src/components/Controlled.jsx';
+import Controlled from './src/components/Controlled.jsx';
 import Uncontrolled from './src/components/Uncontrolled.jsx';
 import Ejemplo from './src/components/Ejemplo.jsx';
 import DynamicInputs from './src/components/DynamicInputs.jsx';
 
 render(<div>
     <h2>Ejemplo</h2>
-    <Ejemplo lang='hun'/>
+    <Ejemplo />
   </div>, document.getElementById('app'));
