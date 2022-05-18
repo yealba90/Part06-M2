96a4c42ed015d70b270aa800d27bf242a3c8421a
Yeison Alvarez Balvin <ya.balvin1990@gmail.com>
2022-05-18 10:53:59 -0500

    SolviNG Homework

--- 11-Redux/homework/src/reducers/index.js
+++ 11-Redux/homework/src/reducers/index.js
@@ -10,9 +10,15 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // completa para este caso
+      // completa para este caso
+      return {
+        count: state.count + 1
+      }
     case DECREMENT:
-    // Fill para este otro
+      // Fill para este otro
+      return {
+        count: state.count - 1
+      }
     default:
       return state;
   }
