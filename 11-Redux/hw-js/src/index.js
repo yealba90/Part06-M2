826e9797de733902d35f8b7ab0cc349bdfc3ae88
Yeison Alvarez Balvin <ya.balvin1990@gmail.com>
2022-05-18 10:52:52 -0500

    SolviNG Homework

--- 11-Redux/hw-js/src/index.js
+++ 11-Redux/hw-js/src/index.js
@@ -1,30 +1,42 @@
 const { createStore } = require('redux');
-const contador = require('./reducer');
-const { incremento, decremento } = require('./actions');
+const reducer = require('./reducer');
+const { incremento, decremento, impar, async } = require('./actions');
 
 // En esta linea creamos nuestro store. Pasandole como parametro nuestro Reducer
-var store;
+var store = createStore(reducer);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.querySelector('#valor');
 
 // Esta funcion nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
-  
+  let actualValue = store.getState().contador;
   // Seteamos el numero obtenido como texto dentro del elemento con id 'valor':
-
+  valor.innerText = actualValue;
 }
 
 // Ejecutamos la funcion 'renderContador':
-
+renderContador();
 
 
 // Nos subscribimos al store pasandole la misma funcion. Asi cada vez que llegue una accion, ejecutamos la funcion:
-
+store.subscribe(renderContador);
 
 
 // Por ultimo, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la accion correspondiente:
+let btnPlus = document.querySelector('#incremento');
+btnPlus.onclick = () => store.dispatch(incremento());
+
+let btnMinus = document.querySelector('#decremento');
+btnMinus.onclick = () => store.dispatch(decremento());
+
+let btnImpar = document.querySelector('#incrementoImpar');
+btnImpar.onclick = () => store.dispatch(impar());
+
+let btnAsync = document.querySelector('#incrementoAsync');
+btnAsync.onclick = () => store.dispatch(async());
+
--- 11-Redux/hw-js/src/reducer/index.js
+++ 11-Redux/hw-js/src/reducer/index.js
@@ -1,4 +1,4 @@
-const { INCREMENTO, DECREMENTO } = require('../action-types');
+const { INCREMENTO, DECREMENTO, IMPAR, ASYNC } = require('../action-types');
 
 const initialState = {
   contador: 0
@@ -9,7 +9,32 @@ const initialState = {
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
 function contador(state = initialState, action) {
-  
+  if(action.type === INCREMENTO){
+    return {
+      contador: state.contador + 1
+    }
+  } 
+  if (action.type === DECREMENTO){
+    return {
+      contador: state.contador - 1
+    }
+  }
+  if (action.type === IMPAR){
+    if (state.contador%2 !== 0){
+      return {
+        contador: state.contador + 1
+      }
+    }
+  }
+  if (action.type === ASYNC){
+    return {
+      contador:     setTimeout(function(){
+        state.contador + 1
+    },1000)
+
+    }
+  }
+  return state;
 }
 
 module.exports = contador;
